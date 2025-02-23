eff76fc8653a74ee081e24c23534d39e2d0a7efd
Alexander Denton <AlexanderTDenton@outlook.com>
2025-02-23 20:44:01 +0000

    Added Size to Beverage class

--- src/Starbuzz/Beverages/Beverage.java
+++ src/Starbuzz/Beverages/Beverage.java
@@ -1,10 +1,21 @@
 package Starbuzz.Beverages;
 
+import Starbuzz.Enums.Size;
+
 public abstract class Beverage {
+    Size size = Size.TALL;
     String description = "Unknown Beverage";
 
     public String getDescription() {
         return description;
+    }
+
+    public void setSize(Size size) {
+        this.size = size;
+    }
+
+    public Size getSize() {
+        return this.size;
     }
 
     public abstract double cost();
