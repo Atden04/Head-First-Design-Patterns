faf4f9e53cbd17d3093421cc2278ba242f4e3a19
Alexander Denton <AlexanderTDenton@outlook.com>
2025-02-23 21:15:54 +0000

    Added new line before method.

--- src/Starbuzz/Decorators/CondimentDecorator.java
+++ src/Starbuzz/Decorators/CondimentDecorator.java
@@ -6,7 +6,7 @@ import Starbuzz.Enums.Size;
 public abstract class CondimentDecorator extends Beverage {
     Beverage beverage;
     public abstract String getDescription();
-    
+
     public Size getSize() {
         return this.beverage.getSize();
     }
