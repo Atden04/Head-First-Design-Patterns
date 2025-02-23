6ede683e8322cac13849e26befe342634fc94516
Alexander Denton <AlexanderTDenton@outlook.com>
2025-02-23 20:47:40 +0000

    Missed getSize from CondimentDecorator

--- src/Starbuzz/Decorators/CondimentDecorator.java
+++ src/Starbuzz/Decorators/CondimentDecorator.java
@@ -1,8 +1,13 @@
 package Starbuzz.Decorators;
 
 import Starbuzz.Beverages.Beverage;
+import Starbuzz.Enums.Size;
 
 public abstract class CondimentDecorator extends Beverage {
     Beverage beverage;
     public abstract String getDescription();
+    
+    public Size getSize() {
+        return this.beverage.getSize();
+    }
 }
